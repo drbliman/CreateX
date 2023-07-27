a4acbe185b4a561fc1a96fbb1d80f93a71f1d09d
drbliman <dr.bliman505@gmail.com>
2023-07-27 13:14:58 +0400

    feat: add header

---
+++ script/scripn_header.js
@@ -0,0 +1,4 @@
+document.getElementById('header_burger').onclick = function() {
+    document.getElementById('header_burger').classList.toggle('active');
+    document.getElementById('header_list').classList.toggle('active');
+}
